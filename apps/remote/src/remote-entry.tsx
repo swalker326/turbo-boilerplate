4b2cd7d18830b9c9881277089a56a16a6fc52a21
shane <shane@swalker.dev>
2025-03-20 23:20:23 -0500

    feat: consume shared UI package.

--- apps/remote/src/remote-entry.tsx
+++ apps/remote/src/remote-entry.tsx
@@ -1,8 +1,11 @@
+import { Button } from "ui";
+
 const RemoteEntry = () => {
   return (
     <div className="bg-green-900 h-full flex flex-col items-center justify-center">
       <h1 className="text-5xl">Remote Application</h1>
       <p>Remotes can be served via module federation</p>
+      <Button>Button from Shared UI</Button>
     </div>
   );
 };
