be7cce7e6c092876d00820b4f9f7644c6abdeb27
shane <shane@swalker.dev>
2025-03-21 05:51:49 -0500

    added zephyr to remote

--- apps/remote/rsbuild.config.ts
+++ apps/remote/rsbuild.config.ts
@@ -1,10 +1,25 @@
-import { defineConfig } from "@rsbuild/core";
+import { defineConfig, type RsbuildPlugin } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
 import { mfConfig } from "./module-federation.config";
 import { pluginModuleFederation } from "@module-federation/rsbuild-plugin";
+import { withZephyr } from "zephyr-rspack-plugin";
+
+const pluginZephyrRsbuild = (): RsbuildPlugin => ({
+  name: "plugin-zephyr-rsbuild",
+  async setup(api) {
+    api.modifyRspackConfig(async (config, { mergeConfig }) => {
+      const zeConfig = await withZephyr()(config);
+      mergeConfig(zeConfig);
+    });
+  }
+});
 
 export default defineConfig({
-  plugins: [pluginReact(), pluginModuleFederation(mfConfig)],
+  plugins: [
+    pluginReact(),
+    pluginModuleFederation(mfConfig),
+    pluginZephyrRsbuild()
+  ],
   server: {
     port: 3002
   }
